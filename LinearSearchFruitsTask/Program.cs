7c18fdc93952d5d082a199231194ea927bedf6e2
PippinRing43 <arthurrdye@gmail.com>
2025-11-16 16:39:07 +0000

    Update Program.cs

--- LinearSearchFruitsTask/Program.cs
+++ LinearSearchFruitsTask/Program.cs
@@ -7,17 +7,8 @@
         static void Main(string[] args)
         {
             string[] fruits = { "banana", "apple", "orange", "pear", "grape", "pineapple" };
-            
             Console.WriteLine("What word do you want to find?");
             Console.WriteLine(Find(fruits, Console.ReadLine()));
-
-            // Your program should:
-            // Ask the user what word they would like to find
-            // Output the message True if the word is found
-            // Output the message False if the word is not found
-            
-            // You must write your own linear search routine and not use any built-in fuctions available in C#
-            // You should use meaningful variable names
         }
         
         static bool Find(string[] array, string key) 
@@ -33,3 +24,11 @@
         }
     }
 }
+
+// Your program should:
+// Ask the user what word they would like to find
+// Output the message True if the word is found
+// Output the message False if the word is not found
+
+// You must write your own linear search routine and not use any built-in fuctions available in C#
+// You should use meaningful variable names
